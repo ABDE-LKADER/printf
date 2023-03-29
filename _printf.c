b45788775f16e16de9822789cfdbd6968ad38454
ABDE-LKADER <abdelkader.adouab@edu.uca.ma>
2023-03-28 17:33:38 -0700

    FIX

--- _printf.c
+++ _printf.c
@@ -3,6 +3,19 @@
 #include "main.h"
 
 /**
+ * _putchar -> Writes the character
+ *
+ * @c: Input Char
+ *
+ * Return: ...
+ */
+
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
+/**
  * _printf -> Printf function
  *
  * @format: Input String
@@ -13,13 +26,28 @@
 int _printf(const char *format, ...)
 {
 	int i;
+	int num;
 	va_list list;
-
-	va_start(list, format);
 	
 	if (format == NULL)
-        return (-1);
-	va_end();
+		return (-1);
+
+	va_start(list, format);
+
+	for (i = 0; format[i] != '\0'; i++)
+	{
+		if (format[i] == '%')
+		{
+			if (format[i] == '\0')
+				return (-1);
+			else if (format[i] == 'd')
+			{
+				num = va_arg(args, int);
+			}
+		}
+		else
+			_puthar(format[i]);
+	}
+	va_end(list);
 	return ();
 }
-
